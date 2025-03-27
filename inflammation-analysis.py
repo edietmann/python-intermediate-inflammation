4a396eea386ae64a61e6cbe869d36f29c315c90f
edietmann <edietmann@wisc.edu>
2025-03-27 12:03:49 -0500

    fixed bugs and added README.md

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -49,11 +49,9 @@ def main(args):
         inflammation_data = models.load_csv(filename)
 
         view_data = {
-        feature-std-dev
-        'average': models.daily_mean(inflammation_data), 
-        'max': models.daily_max(inflammation_data), 
-        'min': models.daily_min(inflammation_data), 
-        **(models.s_dev(inflammation_data))
+            'average': models.daily_mean(inflammation_data),
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)
         }
 
         views.visualize(view_data)
